41f3bcafde272733001d18e5f72b51364ee6d099
Marissa Miner <marissaminer@Marissas-MacBook-Air.local>
2018-06-11 20:23:32 -0500

    getting error for invoking quit

--- bamazon.js
+++ bamazon.js
@@ -67,64 +67,25 @@ function queryAllProducts() {
         connection.query(query, { product: answer.product }, 
             function(err, res) {
                 if (err) throw err;
+                connection.end();
         //   for (var j = 0; j < res.length; j++) {
             if (res[0].stock_quantity < answer.quantity) {
                 console.log("Insufficient quantity!");
                 // console.log("there are", ${res[0].stock_quantity}, "available in stock");
                 // whichProductWouldYouLike();
                 // return;
-
             } else {
                 var totalCost = answer.quantity * res[0].price;
                 console.log("order fulfilled!");
                 // var newStockQuant = res[0].stock_quantity - answer.howMany;
                 // connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newStockQuant, answer.whichId], function(err, res) {
                 //     if (err) throw err;
+
+                // connection.end();
             }
-                });
-            
-      });
+        });
+    });
+};
   
 
   productSearch();
-
-
-//   connection.connect(function(err) {
-//     if (err) throw err;
-//   //   runSearch();
-//   console.log("did this work?");
-//   });
-
-// function runSearch() {
-//   inquirer
-//     .prompt({
-//       name: "action",
-//       type: "list",
-//       message: "What would you like to buy?",
-//       choices: [
-//         "Find songs by artist",
-//         "Find all artists who appear more than once",
-//         "Find data within a specific range",
-//         "Search for a specific song"
-//       ]
-//     })
-//     .then(function(answer) {
-//       switch (answer.action) {
-//       case "Find songs by artist":
-//         artistSearch();
-//         break;
-
-//       case "Find all artists who appear more than once":
-//         multiSearch();
-//         break;
-
-//       case "Find data within a specific range":
-//         rangeSearch();
-//         break;
-
-//       case "Search for a specific song":
-//         songSearch();
-//         break;
-//       }
-//     });
-// }
